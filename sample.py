156a1bc772cc7a2e3dda602237d82826cb3a451a
curio <curio@antique-cafe.net>
2019-07-14 04:12:00 +0900

    fix

--- sample.py
+++ sample.py
@@ -8,17 +8,20 @@ from magictrader.indicator import (ADX, BBANDS, MACD, RSI, SMA, STDDEV,
 
 if __name__ == "__main__":
 
+    # フィーダーを作成する
     feeder = CandleFeeder("btc_jpy", "5m", 200, True, datetime(2019, 3, 1), datetime(2019, 6, 30))
     # feeder = CandleFeeder("btc_jpy", "5m", 200)
 
+    # ローソク足を作成する
     candle = Candle(feeder)
+    # テクニカルインディケーターを作成する
     sma_fast = SMA(feeder, 21, AppliedPrice.CLOSE)
     sma_slow = SMA(feeder, 89, AppliedPrice.CLOSE)
-
     adx_fast = ADX(feeder, 13)
     adx_slow = ADX(feeder, 26)
     stddev = STDDEV(feeder, 20, 1, AppliedPrice.CLOSE)
 
+    # チャート画面(メイン)を作成する
     window_main = ChartWindow()
     window_main.title = "btc_jpy"
     window_main.height_ratio = 3
@@ -26,15 +29,14 @@ if __name__ == "__main__":
     window_main.indicators_left.append(sma_fast)
     window_main.indicators_left.append(sma_slow)
 
+    # チャート画面(サブ)を作成する
     window_sub = ChartWindow()
     window_sub.height_ratio = 1
-    window_sub.ylabel_left = "adx"
-    window_sub.ylabel_right = "stddev"
-    window_sub.legend_visible = True
     window_sub.indicators_left.append(adx_fast)
     window_sub.indicators_left.append(adx_slow)
     window_sub.indicators_right.append(stddev)
 
+    # 画面を登録し、チャートを表示する
     chart = Chart()
     chart.add_window(window_main)
     chart.add_window(window_sub)
