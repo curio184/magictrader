10be9a593a96fa8e599dd8957ce05622b8012da4
curio <curio@antique-cafe.net>
2019-07-18 02:55:04 +0900

    release 0.1.0

--- magictrader/__init__.py
+++ magictrader/__init__.py
@@ -1,5 +1,5 @@
 __copyright__ = 'Copyright (C) 2019 Yusuke Oya'
-__version__ = '0.0.9'
+__version__ = '0.1.0'
 __license__ = 'MIT'
 __author__ = 'Yusuke Oya'
 __author_email__ = 'curio@antique-cafe.net'
